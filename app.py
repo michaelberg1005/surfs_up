4df39319d0f6ae2b517d5715e80ae709194d2c46
michaelberg1005 <mh.berg@comcast.net>
2020-05-08 12:27:50 -0700

    adding completed challenge with climate_analysis file

--- app.py
+++ app.py
@@ -78,17 +78,20 @@ def temp_monthly():
 @app.route("/api/v1.0/temp/<start>/<end>")
 
 def stats(start=None, end=None):
-    sel = [func.min(Measurement.tobs), func.avg(
-        Measurement.tobs), func.max(Measurement.tobs)]
-
+     # Select statement
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
     if not end:
-	    results = session.query(*sel).\
-        filter(Measurement.date <= start).all()
-        temps = list(np.ravel(results))
-        return jsonify(temps)
-
+        # calculate TMIN, TAVG, TMAX for dates greater than start
+            results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        # Unravel results into a 1D array and convert to a list
+            temps = list(np.ravel(results))
+            return jsonify(temps)
+    
+    # calculate TMIN, TAVG, TMAX with start and stop
     results = session.query(*sel).\
     filter(Measurement.date >= start).\
     filter(Measurement.date <= end).all()
+    # Unravel results into a 1D array and convert to a list
     temps = list(np.ravel(results))
     return jsonify(temps)
